e3f7844b3fca0d3fa3fed3cd5a91d635d798124a
Aurimas Liutikas <aurimas@google.com>
2022-04-08 16:49:18 -0700

    Add a small bit of docs

--- plugin/src/main/kotlin/androidx/build/gradle/gcpbuildcache/GcpBuildCache.kt
+++ plugin/src/main/kotlin/androidx/build/gradle/gcpbuildcache/GcpBuildCache.kt
@@ -2,5 +2,9 @@ package androidx.build.gradle.gcpbuildcache
 
 import org.gradle.caching.configuration.AbstractBuildCache
 
+/**
+ * Gradle Build Cache that uses GCP buckets as a backing for load and store
+ * Gradle results.
+ */
 class GcpBuildCache : AbstractBuildCache() {
 }
--- plugin/src/main/kotlin/androidx/build/gradle/gcpbuildcache/GcpBuildCacheService.kt
+++ plugin/src/main/kotlin/androidx/build/gradle/gcpbuildcache/GcpBuildCacheService.kt
@@ -5,6 +5,10 @@ import org.gradle.caching.BuildCacheEntryWriter
 import org.gradle.caching.BuildCacheKey
 import org.gradle.caching.BuildCacheService
 
+/**
+ * The service that responds to Gradle's request to load and store results for a given
+ * [BuildCacheKey].
+ */
 class GcpBuildCacheService : BuildCacheService {
   override fun close() {
     TODO("Not yet implemented")
