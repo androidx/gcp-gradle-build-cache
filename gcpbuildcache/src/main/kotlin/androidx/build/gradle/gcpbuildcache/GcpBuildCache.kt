e0585a45945c9a5bedcb108170fe3b0b715569cb
Aurimas Liutikas <aurimas@google.com>
2022-04-12 19:44:55 -0700

    Move the docs to the right place

--- gcpbuildcache/src/main/kotlin/androidx/build/gradle/gcpbuildcache/GcpBuildCache.kt
+++ gcpbuildcache/src/main/kotlin/androidx/build/gradle/gcpbuildcache/GcpBuildCache.kt
@@ -5,13 +5,17 @@ import org.gradle.caching.configuration.AbstractBuildCache
 /**
  * Gradle Build Cache that uses GCP buckets as a backing for load and store
  * Gradle results.
- *
- * @param projectId The Google Cloud Platform project id, that can be used for billing.
- * @param bucketName The name of the bucket that is used to store all the gradle cache entries.
- * This essentially becomes the root of all cache entries.
  */
 open class GcpBuildCache(
 ) : AbstractBuildCache() {
+  /**
+   * The Google Cloud Platform project id, that can be used for billing.
+   */
   lateinit var projectId: String
+
+  /**
+   * The name of the bucket that is used to store all the gradle cache entries.
+   * This essentially becomes the root of all cache entries.
+   */
   lateinit var bucketName: String
 }
